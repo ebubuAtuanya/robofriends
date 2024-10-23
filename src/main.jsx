29d64ae674352afd06c8f19a5db6edadaa95299f
ebubuAtuanya <107080594+ebubuAtuanya@users.noreply.github.com>
2024-10-23 09:27:42 -0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,13 @@ import { createRoot } from 'react-dom/client'
 import App from './Containers/App.jsx'
 import 'tachyons';
 import './index.css';
+import HashRouter from 'react-router-dom';
 
 
 
 
 createRoot(document.getElementById('root')).render(
-  
+  <HashRouter>
     <App />
-  
+  </HashRouter>
 )
